69e383cc48d3ee7b400190e0f1245907085d739e
sourav-manna <sourav.manna2000@outlook.com>
2022-07-17 23:28:14 +0530

    sourav | commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 setInterval(Clock, 1000);
 function Clock(){
     var today = new Date();
     var hr = today.getHours();
     var min = today.getMinutes();
+    var day = today.getDay();
     if(hr<10){
         hr = "0"+hr
     }
@@ -13,6 +15,7 @@ function Clock(){
     console.log(time)
     document.getElementById("time1").innerHTML = time
     document.getElementById("time2").innerHTML = time
+    document.getElementById("day").innerHTML = days[day]
 }
 
 window.onload = function() { Clock()}
